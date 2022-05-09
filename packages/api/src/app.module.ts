95944b0a461d9be69125859d8d710161aea18f5d
jogaec22 <jogaec22@gmail.com>
2022-05-09 05:07:52 -0500

    feat: new modules in api

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ManageApiModule } from './manage-api/manage-api.module';
+import { ManageUiModule } from './manage-ui/manage-ui.module';
 
 @Module({
-  imports: [],
+  imports: [ManageApiModule, ManageUiModule],
   controllers: [AppController],
   providers: [AppService],
 })
